a84350b0bc31abe0dae787dbee1e8c12a1b80d54
Adnan <technicaljigsaw.tj@gmail.com>
2025-08-30 20:41:56 +0530

    fix: removed unneccesary memory checking

--- backend/api/index.js
+++ backend/api/index.js
@@ -114,16 +114,16 @@ mongoose.connect(dbURI)
   });
 
    // check memory usage (local-dev)
-   const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`;
-   const memoryData = process.memoryUsage();
-   const memoryUsage = {
-     rss: `${formatMemoryUsage(memoryData.rss)} -> Resident Set Size - total memory allocated for the process execution`,
-     heapTotal: `${formatMemoryUsage(memoryData.heapTotal)} -> total size of the allocated heap`,
-     heapUsed: `${formatMemoryUsage(memoryData.heapUsed)} -> actual memory used during the execution`,
-     external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`,
-   };
+  //  const formatMemoryUsage = (data) => `${Math.round(data / 1024 / 1024 * 100) / 100} MB`;
+  //  const memoryData = process.memoryUsage();
+  //  const memoryUsage = {
+  //    rss: `${formatMemoryUsage(memoryData.rss)} -> Resident Set Size - total memory allocated for the process execution`,
+  //    heapTotal: `${formatMemoryUsage(memoryData.heapTotal)} -> total size of the allocated heap`,
+  //    heapUsed: `${formatMemoryUsage(memoryData.heapUsed)} -> actual memory used during the execution`,
+  //    external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`,
+  //  };
    
-   console.log(memoryUsage);
+  //  console.log(memoryUsage);
 
 httpserver.listen(port, () => {
   // console.log(`Server is running on port ${port}`);
